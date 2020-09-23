5e2946af948b8b0041658d70c312988d85c70a3f
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-23 19:38:57 +0930

    update index/ javascript

---
+++ javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    function menu(){
+        $('#menu-container').css('display', 'block');
+        $('.close').css('display', 'flex')
+    };
+
+
+
+
+
+
+}); //end document ready 
