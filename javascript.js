0f9e24b4802afbec95402a32f31b0f54c2cca972
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-26 13:58:16 +0930

    add functional start and finish button with countdown display

--- javascript.js
+++ javascript.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     $('#hamburger').on("click", function(){
         $('#menu-container').css('display', 'block');
         $('.close').css('display', 'flex');
-        
     });
 
     function bmiCalc() {
@@ -75,18 +74,93 @@ $(document).ready(function(){
     });
 
 
-    var progressBar = new ProgressBar.Circle('#progress', {
-        color: '#FFCC00',
-        strokeWidth: 5,
-        trailWidth: 0.8,
-        svgStyle: {
-            display: 'block',
-            width: '100%'
-        },
-        fill: 'rgba(255, 255, 50, 1)',
-        stroke: 2,
-        duration: 1200,
-        warnings: true
+    // timer
+
+    var fasting = false;
+    var startTime = "";
+    var startDay = "";
+    var endTime = "";
+    var endDay = "";
+
+    // get value of fasting type
+    var fastType = $('#type-selector').val();
+
+    // add button to page'
+    if (fasting === false){
+        $('#start-btn').css("display", 'block');
+        $('#end-btn').css('display', 'none');
+    } else {
+        $('#start-btn').css("display", 'none');
+        $('#end-btn').css('display', 'block');
+    }
+
+var startMilli; 
+    // start fast
+    $('#start-btn').on("click", function(){
+            startTime = moment().format('HH:mm:ss');
+            startMilli = new Date(Date.UTC());
+            startDay = moment().format('dd');
+            endTime = moment().add(fastType, 'hours').format('HH:mm');
+            endDay = moment().add(fastType, 'hours').format('dd');
+            fasting=true;
+            // show end fast button
+            $('#start-btn').css("display", 'none');
+            $('#end-btn').css('display', 'block');
+    }); 
+
+    var refreshTimer;
+
+    refreshTimer = setInterval(function(){
+        if (startDay==moment().format('dd')){
+            $('#start-time').text("Today "+startTime);
+            $('#end-time').text("Tomorrow "+endTime);
+        }
+        var timeDisplay = moment().subtract(startTime).format('HH:mm:ss');
+        var elapsedTime = parseInt(fastType) - parseInt(timeDisplay);
+        if (fasting==true){
+        $('#digits-display').text(timeDisplay);
+        $('#elapsed-time').text(elapsedTime);
+        }
+    }, 500);
+    
+        // function to show end fast screen
+        
+    function endFast(){
+        $('main').css('display', 'none');
+        $('#end-fast-screen').css('display', 'block');
+        var fastEnded = moment().subtract(startTime).format('HH:mm:ss');
+        $('#final-time').text(fastEnded);
+        console.log("end fast working");
+        // reset variables 
+        fasting = false;
+        startTime = "";
+        startDay = "";
+        endTime = "";
+        endDay = "";
+    }
+    // close end screen
+    $('#close-fast-screen').on("click", function(){
+        $('#end-fast-screen').css('display', 'none');
+        $('main').css('display', 'block');
     });
+    
+    $('#end-btn').on("click", function(){
+        endFast();
+    });
+    // add this v back in if I get time
+
+    // // display today or yesterday in start day
+    // if (startDay==moment().format('dd')){
+    //     $('#start-day option:selected').val("Today");
+    // } else {
+    //     $('#start-day option:selected').val("Yesterday");
+    // }
+    // // display end day - today or tomorrow
+    // if (endDay==moment().format('dd')){
+    //     $('#end-day option:selected').val("Today");
+    // } else {
+    //     $('#end-day option:selected').val("Tomorrow");
+    // }
+    // // change start day and time if user changes it.
 
 }); //end document ready 
