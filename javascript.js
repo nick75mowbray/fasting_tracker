265fd4ccb8a78cd1f54da867e20c6f7ae0d11437
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-30 13:25:18 +0930

    move bmi and calorie calculator to own html pages, add some styling to calorie calc, fix menu pop-up, fix timer display after ending fast

--- javascript.js
+++ javascript.js
@@ -2,57 +2,29 @@ $(document).ready(function(){
     console.log("js file working");
     
     // open hamburger menu
-    $('#hamburger').on("click", function(){
-        $('#menu').css('left', '0px');
-        $('.close').css('display', 'flex');
-    });
+    function openMenu(){
+        $('#hamburger').css("display", "none");
+        $('#menu').css("display", "block");
+    }
+    // add openMenu to onclick
+    $('#hamburger').on("click", openMenu);
     
     //close hamburger menu
-    $('#close').on("click", function(){
-        $('#menu').css('left', "-300px");
+    function closeMenu(){
+        $('#menu').css("display", "none");
         $('#hamburger').css('display', 'block');
-    });
+    }
+    // add closeMenu to onclick
+    $('#close').on("click", closeMenu);
+
+// close menu when opening new page
+$('li.menu-li').on("click", closeMenu);
 
     //BMI Calculator
     function bmiCalc() {
 
-        //$('#heading').empty();
-        //$('#bmi-div').empty();
-        //$('#results').empty();
-        
-
-        //heading
-        $('#heading').append('<h1 class="is-size-1" id="BMI-header">BMI Calculator</h1>');
-        $('#heading').append('<p class="is-size-3 has-text-weight-light firstPara" id="BMI-para">Calculate your body mass index with your age, weight and height!</p>');
-        $('#BMI-para').append('<hr>');
-
-        //form
-        $('#bmi-div').append('<label class="label" for="age" id="ageLabel">Age </label>');
-        $('#ageLabel').append('<input type="text" id="age" class="BMIinput"></input>');
-        $('#ageLabel').append('<br>');
-        $('#bmi-div').append('<label class="label" for="height" id="heightLabel">Height </label>');
-        $('#heightLabel').append('<input type="text" id="height" class="BMIinput"></input>');
-        $('#heightLabel').append('<br>');
-        $('#bmi-div').append('<label class="label" for="weight" id="weightLabel">Weight </label>');
-        $('#weightLabel').append('<input type="text" id="weight" class="BMIinput"></input>');
-        $('#weightLabel').append('<br>');
-
-        $('#bmi-div').append('<button class="submit button is-outlined is-success" id="submitBTN">submit</button>');
-
         $('#submitBTN').on('click', function(event) {
             event.preventDefault();
-            
-            // open hamburger menu
-            $('#hamburger').on("click", function(){
-            $('#menu').css('left', '0px');
-            $('.close').css('display', 'flex');
-            });
-        
-                //close hamburger menu
-            $('#close').on("click", function(){
-            $('#menu').css('left', "-300px");
-            $('#hamburger').css('display', 'block');
-            });
 
             $('#results').empty();
 
@@ -65,11 +37,11 @@ $(document).ready(function(){
             console.log(weight);
 
             var settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": "https://fitness-calculator.p.rapidapi.com/bmi?age=" + age + "&height=" + height + "&weight=" + weight + "",
-                "method": "GET",
-                "headers": {
+                async: true,
+                crossDomain: true,
+                url: "https://fitness-calculator.p.rapidapi.com/bmi?age=" + age + "&height=" + height + "&weight=" + weight + "",
+                method: "GET",
+                headers: {
                     "x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
                     "x-rapidapi-key": "9412a276cbmsh4a03f98b46a37d7p1d53c7jsnbbec000e7aac"
                 }
@@ -94,23 +66,11 @@ $(document).ready(function(){
         });
 
     };
+    // run Bmi calc
+    bmiCalc();
 
 
     function calorieCalc(){
-        //Ingradient
-        $('#cal-div').append('<label for="ingradient" id="IngradientLabel">Ingradient</label>');
-        $('#IngradientLabel').append('<input type="text" id="Ingradient"></input>');
-        $('#IngradientLabel').append('<br>');
-      //  $('#IngradientLabel').append('<small>Format:1 large apple</small>');             
-      //  $('#IngradientLabel').append('<br>');
-
-        
-        $('#cal-div').append('<label for="quantity" id="IngradientQuantity">Quantity</label>');
-        $('#IngradientQuantity').append('<input type="number" id="Quantity"></input>');
-        $('#IngradientQuantity').append('<br>');
-        
-
-        $('#cal-div').append('<button id="submitBTN">submit</button>');
 
         $('#submitBTN').on('click', function(event) {
             event.preventDefault();
@@ -120,14 +80,11 @@ $(document).ready(function(){
             console.log(quantity);
 
             var settings = {
-                "async": true,
-                "crossDomain": true,
-                //"url": "https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1%20large%20apple",
-               // "url": "https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=" + ingrd + "",
-                  "url": "https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=" +quantity + "%20" +ingrd + "",
-                             
-                "method": "GET",
-                "headers": {
+                async: true,
+                crossDomain: true,
+                url: "https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=" +quantity + "%20" +ingrd + "",
+                method: "GET",
+                headers: {
                     "x-rapidapi-host": "edamam-edamam-nutrition-analysis.p.rapidapi.com",
                     "x-rapidapi-key": "0157ded83emshe3bc01cf91c87e5p150d98jsnc0f8cffc1c49"
                 }
@@ -142,30 +99,12 @@ $(document).ready(function(){
             });
         })
     }
-
-    // show BMI calculator
-    $('#BMI-li').on("click", function(){
-        console.log('BMI Calculator');
-        bmiCalc();
-        $('main').css('display', 'none');
-        $('#menu-container').css('display', 'none');
-    });
-
-     // show calorie calculator
-
-     $('#calorie-li').on("click",function(){
-        calorieCalc();
-        $('main').css('display', 'none');
-        $('#menu-container').css('display', 'none');
-    });
-
-    //close hamburger menu
-    $('#close').on("click", function(){
-        $('#menu-container').css('display', 'none');
-        $('#hamburger').css('display', 'block');
-    });
+// run calorie calculator
+calorieCalc();
    
-    // LOCAL STORAGE
+    
+
+// LOCAL STORAGE
 var previousFasts = [];
 var start = {minutes: "", hours: "", day: "", type: "", endtime: "", endday: "", fasting: false};
 
@@ -209,14 +148,9 @@ function storeCurrrentFast() {
     localStorage.setItem("currentFast", JSON.stringify(start));
 };
 
-    // TIMER
+    
 
-    var fasting = false;
-    var startTime = "";
-    var startMinutes = "";
-    var startDay = "";
-    var endTime = "";
-    var endDay = "";
+// TIMER
 
     // get value of fasting type
     start.type = $('#type-selector').val();
@@ -238,6 +172,8 @@ function storeCurrrentFast() {
  
     // start fast
     $('#start-btn').on("click", function(){
+            // get value of fasting type
+            start.type = $('#type-selector').val();
             start.hours = moment().format('HH:mm:ss');
             start.minutes = moment().format('mm');
             start.day = moment().calendar();
@@ -305,7 +241,18 @@ function storeCurrrentFast() {
             previousFasts.unshift(parseInt(fastEnded));
         }
         storePrevious();
-    }
+        // reset display
+        $('#digits-display').text("00:00:00");
+        $('#elapsed-time').text("");
+        // show start button
+        // show end fast button
+        $('#start-btn').css("display", 'block');
+        $('#end-btn').css('display', 'none');
+        
+    }    
+    $('#end-btn').on("click", function(){
+        endFast();
+    });
 
     // previous fasts graph
     function showPreviousFasts(){
@@ -333,9 +280,7 @@ function storeCurrrentFast() {
         $('main').css('display', 'block');
     });
     
-    $('#end-btn').on("click", function(){
-        endFast();
-    });
+
 
 
 
